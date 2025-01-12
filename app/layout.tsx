7ae522a96a40c1ed42cf1fc62d591c4d1c22f9fe
austinbowers <austin.bowers75@gmail.com>
2025-01-12 15:09:26 -0600

    Add Speed Insights from Vercel.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           </Link>
         </header>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
