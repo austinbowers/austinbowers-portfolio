5e6a96b1988e8fd6724c432b163a071ae3b0518a
austinbowers <austin.bowers75@gmail.com>
2025-02-27 14:02:09 -0600

    Fix form input styles.

--- app/ui/global/formcarry.tsx
+++ app/ui/global/formcarry.tsx
@@ -49,29 +49,29 @@ export default function BasicForm() {
 
             <div className="formcarry-block mb-6">
                 <label
-                    className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
+                    className={`block mb-2 text-sm font-medium text-gray-300`}
                     htmlFor="name">Full Name</label>
                 <input
-                    className={`shadow-sm dark:bg-opacity-50 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light`}
+                    className={`shadow-sm bg-opacity-50  border  text-sm rounded-lg outline-none  block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 shadow-sm-light`}
                     type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" placeholder="Your first and last name" />
             </div>
 
             <div className="formcarry-block mb-6">
                 <label
-                    className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
+                    className={`block mb-2 text-sm font-medium text-gray-900 text-gray-300`}
                     htmlFor="email">Your Email Address</label>
                 <input
-                    className={`shadow-sm dark:bg-opacity-50 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light`}
+                    className={`shadow-sm bg-opacity-50  border  text-sm rounded-lg outline-none  block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 shadow-sm-light`}
                     type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="john@doe.com" />
             </div>
 
             <div className="formcarry-block mb-6">
                 <label
-                    className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
+                    className={`block mb-2 text-sm font-medium text-gray-900 text-gray-300`}
                     htmlFor="message">Your message</label>
                 <textarea
                     rows={6}
-                    className={`shadow-sm dark:bg-opacity-50 outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light`}
+                    className={`shadow-sm bg-opacity-50 outline-none  border  text-sm rounded-lg  block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 shadow-sm-light`}
                     value={message} onChange={(e) => setMessage(e.target.value)} id="message" placeholder="Enter your message..."></textarea>
             </div>
 
